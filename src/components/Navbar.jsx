d78f79563202c930c18c1a30e32f412f8371c47e
Sabrina Maria Andrade Carvalho <sabrinaibm@MacBook-Pro-de-Sabrina.local>
2022-11-18 16:17:50 -0300

    primeiro commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { GiGalaxy } from "react-icons/gi"
+import { BiSearchAlt } from "react-icons/bi"
+
+import "../style/NavBar.css"
+
+const Navbar = () => {
+    
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!search) return;
+
+        navigate(`/search?q=${search}`);
+        setSearch("");  
+    };
+
+    return (
+        <nav id="navbar">
+            <h2>
+                <Link to="/"><GiGalaxy/>Galaxy Movies</Link>
+            </h2>
+            <form onSubmit={handleSubmit}>
+               <input 
+                type="text" 
+                placeholder="Busque um filme" 
+                onChange={(e) => setSearch(e.target.value)}
+                value={search}
+               />
+               <button type="submit">
+                   <BiSearchAlt/>
+                </button> 
+            </form>
+        </nav>
+    )
+  }
+  
+  export default Navbar
